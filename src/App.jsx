4c8daa0dcbc67e1aca9eaa330bdd8e95570506f3
Ajknight121 <aknig2@uic.edu>
2024-01-23 16:50:38 -0600

    Created better userCard with img

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ function App() {
 
   return (
     <>
-    <AdrianKnight/>
-    <AdrianKnight/>
+      <AdrianKnight/>
     </>
     
     )
--- src/components/Contributors.jsx
+++ src/components/Contributors.jsx
@@ -1,10 +1,20 @@
+import profilePicAdrian from "../images/Arrow Swarm trails.png"
 
-export function AdrianKnight() {
 
+export const AdrianKnight = ( ) => {
     return (
-        <div className="bg-grey-500 ">
-            <h4 className="underline">Adrian Knight</h4>
-            <div>@Ajknight121</div>
+      <div>
+        <div className="flex justify-center align-middle bg-purple-500 shadow-contributor max-w-[380px] h-[100px] m-5 border-solid border-2 border-black" >
+          <img alt="profile picture of Adrian Knight" className="rounded-[50%] border-indigo-600 border-[4px] -translate-x-[20%] -translate-y-4 hover:animate-ping delay-1000" src={profilePicAdrian} width={100} height={100} />
+          <div className="text-black text-center w-full self-center mr-5">
+            <h3 className="text-2xl  ">
+              Adrian Knight
+            </h3>
+            <div>
+              <a href={"https://github.com/Ajknight121"}>@Ajknight121</a>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    );
+  };
