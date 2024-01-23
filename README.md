11854fa76e5edc5ae51ae951ffa003990ab50d53
Ajknight121 <aknig2@uic.edu>
2024-01-23 16:22:34 -0600

    Added my card example and Readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Jukebox
+
+
+List of contributors:
+Adrian Knight - [@Ajknight121](https://github.com/Ajknight121)
---
+++ src/components/Contributors.jsx
@@ -0,0 +1,10 @@
+
+export function AdrianKnight() {
+
+    return (
+        <div className="bg-grey-500 ">
+            <h4 className="underline">Adrian Knight</h4>
+            <div>@Ajknight121</div>
+        </div>
+    )
+}
