12a659c41f5c783998f299fd79524e3b30ba26f7
revenge789 <128012388+revenge789@users.noreply.github.com>
2024-02-27 16:50:19 -0600

    created basic overlay and testing for component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@
 import './App.css'
 import { AdrianKnight } from './components/Contributors'
 import { OwenNeal } from './components/Contributors'
+import StatVideoCard from './components/StatVideoCard'
 
 function App() {
-
+  const video = {
+    title: 'Title',
+    length: '387'
+  }
   return (
     <>
       <AdrianKnight/>
       <OwenNeal/>
+      <StatVideoCard video={video}/>
     </>
     )
   }
