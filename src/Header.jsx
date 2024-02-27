feef25314b8d2d9e687f21510d4ef40a3e62207c
eky2429 <eky2@uic.edu>
2024-02-27 16:32:10 -0600

    Fixed issue with image
    O Made image element a button instead of a plain image

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,9 @@ export const Header = () => {
         <>
         <nav className = "flex flex-row justify-around items-center w-screen h-24 text-4xl h-150px font-black bg-neutral-900 text-white">
             <h1 className=" absolute center"> ACM@UIC JUKEBOX </h1>
-            <img className = "h-20 w-20 absolute right-5" src = "src/images/ProfilePic.png"/>
+            <button>
+                <img className = "h-20 w-20 absolute right-5" src = "src/images/ProfilePic.png"/>
+            </button>
         </nav>
         </>
     )
