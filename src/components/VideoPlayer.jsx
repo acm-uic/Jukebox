88a69318dd2d6517caec033285c67dd7e717e9f5
Samuel Skean <sskean2@uic.edu>
2024-02-28 16:21:09 -0600

    Fixed formatting.
    Added parentheses around return value.
    Changed indentation to 2 spaces to match rest of project.

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -2,27 +2,29 @@ import { useState } from 'react'
 import ReactPlayer from 'react-player';
 
 export const VideoPlayer = () => {
-    // Highly informed by this w3schools tutorial:
-    // https://www.w3schools.com/react/react_forms.asp
-    const [playerUrl, setPlayerUrl] = useState("");
-    const [inputUrl, setInputUrl] = useState("");
+  // Highly informed by this w3schools tutorial:
+  // https://www.w3schools.com/react/react_forms.asp
+  const [playerUrl, setPlayerUrl] = useState("");
+  const [inputUrl, setInputUrl] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setPlayerUrl(inputUrl);
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPlayerUrl(inputUrl);
+  }
 
-    return <>
-        <ReactPlayer url={playerUrl}/>
-        <form onSubmit={handleSubmit}>
-            <label> Enter a video URL:
-                <input
-                    type="text"
-                    value={inputUrl}
-                    onChange={(e) => setInputUrl(e.target.value)}
-                />
-                <input type="submit"/>
-            </label>
-        </form>
+  return (
+    <>
+      <ReactPlayer url={playerUrl} />
+      <form onSubmit={handleSubmit}>
+        <label> Enter a video URL:
+          <input
+            type="text"
+            value={inputUrl}
+            onChange={(e) => setInputUrl(e.target.value)}
+          />
+          <input type="submit" />
+        </label>
+      </form>
     </>
+  )
 }
