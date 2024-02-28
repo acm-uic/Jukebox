722cd59ce5077bd94bb7f37fcae9469f6b63bac7
revenge789 <128012388+revenge789@users.noreply.github.com>
2024-02-27 19:43:07 -0600

    few tweaks with color

--- src/components/StatVideoCard.jsx
+++ src/components/StatVideoCard.jsx
@@ -3,14 +3,14 @@ import React, { useState } from 'react'
 export default function StatVideoCard({video}) {
     const [url, setUrl] = useState(video.url)
   return (
-    <div className='w-full h-24 bg-[#464545] flex justify-between items-center text-white p-2'>
+    <div className='w-full h-24 bg-[#464545] flex justify-between items-center text-[#ffffff] p-3'>
         <div className=''>
             <div className='text-5xl font-bold'>{video.title}</div>
             <div>
             <div className='text-sm'>https://www.youtube.com/</div>
             </div>
         </div>
-            <div className='h-full flex flex-col items-end'>
+            <div className="h-full flex flex-col items-end">
                 <div className='text-sm font-bold h-1/4'>LENGTH: XX:XX</div>
                 <button className='text-2xl font-bold h-3/4'>ADD TO QUEUE</button>
             </div>
