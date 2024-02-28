fb7ed467e33c8630dc62cc1ba648875264d0bd57
Samuel Skean <sskean2@uic.edu>
2024-02-27 23:19:15 -0600

    Video URL only updates on form submission.

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -2,18 +2,27 @@ import { useState } from 'react'
 import ReactPlayer from 'react-player';
 
 export const VideoPlayer = () => {
-    const [url, setUrl] = useState("");
+    // Highly informed by this w3schools tutorial:
+    // https://www.w3schools.com/react/react_forms.asp
+    const [playerUrl, setPlayerUrl] = useState("");
+    const [inputUrl, setInputUrl] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setPlayerUrl(inputUrl);
+    }
 
     return <>
         <h1>Hi!</h1>
-        <ReactPlayer url={url}/>
-        <form>
+        <ReactPlayer url={playerUrl}/>
+        <form onSubmit={handleSubmit}>
             <label> Enter a video URL:
                 <input
                     type="text"
-                    value={url}
-                    onChange={(e) => setUrl(e.target.value)}
+                    value={inputUrl}
+                    onChange={(e) => setInputUrl(e.target.value)}
                 />
+                <input type="submit"/>
             </label>
         </form>
     </>
