9603b3b042bd19bb1bac0a7e58cb9169c91943a8
revenge789 <128012388+revenge789@users.noreply.github.com>
2024-03-05 15:34:37 -0600

    Update StatVideoCard.jsx

--- src/components/StatVideoCard.jsx
+++ src/components/StatVideoCard.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 export default function StatVideoCard({video}) {
     const {title, length, url} = video
-
+    const [showUrl, setShowUrl] = useState(false)
     function handleClick(){
       // Add what happens if the Queue button is pressed
     }
@@ -12,14 +12,16 @@ export default function StatVideoCard({video}) {
     <div className='w-full h-[98px] bg-[#464545] flex justify-between items-center text-[#ffffff] py-2 px-3 rounded'>
       <div className='h-full flex flex-col gap-1'>
         <div className='text-5xl font-bold'>{title}</div>
-        <CopyToClipboard text={url}>
-          <button className='text-sm flex items-center gap-1 active:scale-90 active:text-gray-500 transition duration-100 ease-in-out'>
+        <div className='flex items-center'>
+          <button onClick={() => setShowUrl(!showUrl)}className='text-sm active:scale-90 active:text-gray-500 transition duration-100 ease-in-out'>
             <img src='/copyIcon.png'/>
-            <div>
-             {url}
-            </div>
           </button>
-        </CopyToClipboard>
+          {showUrl && <CopyToClipboard text={url}>
+            <button className='text-sm active:scale-90 active:text-gray-500 transition duration-100 ease-in-out'>
+             {url}
+            </button>
+          </CopyToClipboard>}
+        </div>
       </div>
       <div className="h-full flex flex-col items-end justify-between">
         <div className='text-sm font-bold h-1/4'>{`LENGTH: ${length}`}</div>
