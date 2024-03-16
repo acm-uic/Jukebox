552edecff1444f97ae82942f7ad7bd93e1c9ea4a
revenge789 <128012388+revenge789@users.noreply.github.com>
2024-03-15 20:41:24 -0500

    made Stat component and Stat page responsive

--- src/components/StatVideoCard.jsx
+++ src/components/StatVideoCard.jsx
@@ -1,37 +1,48 @@
-import React, { useState } from 'react'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
+import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-export default function StatVideoCard({video}) {
+export default function StatVideoCard({ video }) {
   //taking title as string, length as seconds, url as string
-  const {title, length, url} = video
-  const lengthString = new Date(length * 1000).toISOString().substring(14,19)
+  const { title, length, url } = video;
+  const lengthString = new Date(length * 1000).toISOString().substring(14, 19);
 
-  const [showUrl, setShowUrl] = useState(false)
+  const [showUrl, setShowUrl] = useState(false);
 
-  function handleClick(){
+  function handleClick() {
     // Add what happens if the Queue button is pressed
-
   }
 
   return (
-    <div className='w-full lg:w-[862px] lg:h-[108px] bg-[#464545] flex justify-between items-center text-[#ffffff] py-2 px-3 rounded'>
-      <div className='h-full flex flex-col gap-1'>
-        <div className='lg:text-5xl sm:text-4xl text-3xl font-bold'>{title}</div>
-        <div className='flex items-center'>
-          <button onClick={() => setShowUrl(!showUrl)}className='text-sm active:scale-90 active:text-gray-500 transition duration-100 ease-in-out'>
-            <img src='/copyIcon.png'/>
+    <div className="w-full md:h-[108px] bg-[#464545] flex flex-col md:flex-row md:gap-0 gap-2 justify-between items-center text-[#ffffff] py-2 px-3 rounded font-bold">
+      <div className="h-full w-full flex flex-col md:justify-around md:w-3/4">
+        <div className="block md:max-w md:text-4xl text-3xl overflow-hidden whitespace-nowrap text-ellipsis">
+          {title}
+        </div>
+        <div className="flex items-center">
+          <button
+            onClick={() => setShowUrl(!showUrl)}
+            className="text-sm active:scale-90 active:text-gray-500 transition duration-100 ease-in-out"
+          >
+            <img src="/copyIcon.png" />
           </button>
-          {showUrl && <CopyToClipboard text={url}>
-            <button className='text-sm active:scale-90 active:text-gray-500 transition duration-100 ease-in-out'>
-             {url}
-            </button>
-          </CopyToClipboard>}
+          {showUrl && (
+            <CopyToClipboard text={url}>
+              <button className="text-sm active:scale-90 active:text-gray-500 transition duration-100 ease-in-out">
+                {url}
+              </button>
+            </CopyToClipboard>
+          )}
         </div>
       </div>
-      <div className="h-full flex flex-col items-end justify-between">
-        <div className='text-sm font-bold h-1/4'>{`LENGTH: ${lengthString}`}</div>
-        <button onClick={handleClick} className='text-2xl font-bold h-3/4 active:scale-90 active:text-gray-500 transition duration-100 ease-in-out'>ADD TO QUEUE</button>
+      <div className="w-full h-full flex flex-col md:items-end justify-between md:w-1/4">
+        <div className="text-sm h-1/4">{`LENGTH: ${lengthString}`}</div>
+        <button
+          onClick={handleClick}
+          className="text-2xl h-3/4 active:scale-90 active:text-gray-500 transition duration-100 ease-in-out"
+        >
+          ADD TO QUEUE
+        </button>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/StatsPage.jsx
+++ src/pages/StatsPage.jsx
@@ -1,78 +1,135 @@
-import React, { useState } from 'react'
-import StatVideoCard from '../components/StatVideoCard'
+import React, { useState } from "react";
+import StatVideoCard from "../components/StatVideoCard";
 
 export default function StatsPage() {
-    //using plays as int, likes as int, and recency as an int
-    const videoList = [
-        {title:'Do I Wanna Know?', url:'https://www.youtube.com/watch?v=bpOSxM0rNPM', length: 365, plays: 1627943000, likes:9600000, recency:3},
-        {title:'As It Was', url:'https://www.youtube.com/watch?v=H5v3kku4y6Q', length: 165, plays: 698605451, likes:7900000, recency:2},
-        {title:'The Monkey Dance', url:'https://www.youtube.com/watch?v=qQB-V4r-uMY', length: 139, plays: 19623952, likes:10000, recency:1}, 
-        {title:'I KNOW ?', url:'https://www.youtube.com/watch?v=X7aF3nZOS98', length: 226, plays: 14728895, likes:489000, recency:4}
-    ]
+  //using plays as int, likes as int, and recency as an int
+  const videoList = [
+    {
+      title: "Do I Wanna Know?hbjhbiuhnuinuhijdklaskdlsadjlaks",
+      url: "https://www.youtube.com/watch?v=bpOSxM0rNPM",
+      length: 365,
+      plays: 1627943000,
+      likes: 9600000,
+      recency: 3,
+    },
+    {
+      title: "As It Was",
+      url: "https://www.youtube.com/watch?v=H5v3kku4y6Q",
+      length: 165,
+      plays: 698605451,
+      likes: 7900000,
+      recency: 2,
+    },
+    {
+      title: "The Monkey Dance",
+      url: "https://www.youtube.com/watch?v=qQB-V4r-uMY",
+      length: 139,
+      plays: 19623952,
+      likes: 10000,
+      recency: 1,
+    },
+    {
+      title: "I KNOW ?",
+      url: "https://www.youtube.com/watch?v=X7aF3nZOS98",
+      length: 226,
+      plays: 14728895,
+      likes: 489000,
+      recency: 4,
+    },
+  ];
 
-    // History == 0, Most Played == 1, Most Liked == 2
-    const [currentTab, setCurrentTab] = useState(0)
+  // History == 0, Most Played == 1, Most Liked == 2
+  const [currentTab, setCurrentTab] = useState(0);
 
-    function compareByLikes(a, b){
-        return a.likes - b.likes
-    }
+  function compareByLikes(a, b) {
+    return a.likes - b.likes;
+  }
 
-    function compareByPlays(a, b){
-        return a.plays - b.plays
-    }
+  function compareByPlays(a, b) {
+    return a.plays - b.plays;
+  }
 
-    function compareByRecency(a, b){
-        return a.recency - b.recency
-    }
+  function compareByRecency(a, b) {
+    return a.recency - b.recency;
+  }
 
-  /*return (
-    <div className='min-h-screen w-full flex-grow bg-[#262425] flex flex-col items-center text-[#FFFFFF] py-[71px]'>
-        <div className='flex text-center lg:gap-10 gap-2 lg:text-5xl text-4xl w-full lg:w-[991px] justify-between py-2 font-extrabold mb-[37px] px-2 sm:px-20'>
-            <div
-            onClick={() => {setCurrentTab(0)}}
-            className={
-                currentTab == 0 ?
-                    'hover:cursor-pointer text-[#F6CF00] underline hover:text-yellow-700 hover:no-underline'    
-                    :'hover:cursor-pointer hover:text-[#F6CF00] hover:underline'
-            }>
-                History
-            </div>
-            <div
-            onClick={() => {setCurrentTab(1)}}
-            className={
-                currentTab == 1 ?
-                    'hover:cursor-pointer text-[#F6CF00] underline hover:text-yellow-700 hover:no-underline'    
-                    :'hover:cursor-pointer hover:text-[#F6CF00] hover:underline'
-            }>
-                Most Played
-            </div>
-            <div
-            onClick={() => {setCurrentTab(2)}}
-            className={
-                currentTab == 2 ?
-                    'hover:cursor-pointer text-[#F6CF00] underline hover:text-yellow-700 hover:no-underline'    
-                    :'hover:cursor-pointer hover:text-[#F6CF00] hover:underline'
-            }>
-                Most Liked
-            </div>
+  return (
+    <div className="min-h-screen w-full flex-grow bg-[#262425] flex flex-col items-center text-[#FFFFFF] md:py-[71px] py-8">
+      <div className="flex text-center lg:text-5xl md:text-4xl lg:gap-10 gap-2 text-xl w-full max-w-[991px] justify-around py-2 font-extrabold mb-[37px] ">
+        <div
+          onClick={() => {
+            setCurrentTab(0);
+          }}
+          className={
+            currentTab == 0
+              ? "hover:cursor-pointer text-[#F6CF00] underline hover:text-yellow-700 hover:no-underline"
+              : "hover:cursor-pointer hover:text-[#F6CF00] hover:underline"
+          }
+        >
+          History
         </div>
-        <div className='flex flex-col gap-9 relative'>  
-            {currentTab == 0 && videoList.sort(compareByRecency).map((vid, index) =>
-                <StatVideoCard key={index} video={vid} />
-            )}
-            {currentTab == 1 && videoList.sort(compareByPlays).reverse().map((vid, index) =>
-            <div key={index} className='flex-col flex items-center lg:gap-9 gap-1'>
-                <StatVideoCard key={index} video={vid} />
-                <div className='lg:absolute lg:-left-1/4 text-2xl font-extrabold'>{`Plays: ${vid.plays}`}</div>       
-            </div>
-            )}
-            {currentTab == 2 && videoList.sort(compareByLikes).reverse().map((vid, index) =>
-            <div key={index} className='flex items-center gap-9'>
-                <div className='absolute -left-1/4 text-2xl font-extrabold'>{`Likes: ${vid.likes}`}</div>       
-                <StatVideoCard key={index} video={vid} />
-            </div>
-            )}
+        <div
+          onClick={() => {
+            setCurrentTab(1);
+          }}
+          className={
+            currentTab == 1
+              ? "hover:cursor-pointer text-[#F6CF00] underline hover:text-yellow-700 hover:no-underline"
+              : "hover:cursor-pointer hover:text-[#F6CF00] hover:underline"
+          }
+        >
+          Most Played
         </div>
+        <div
+          onClick={() => {
+            setCurrentTab(2);
+          }}
+          className={
+            currentTab == 2
+              ? "hover:cursor-pointer text-[#F6CF00] underline hover:text-yellow-700 hover:no-underline"
+              : "hover:cursor-pointer hover:text-[#F6CF00] hover:underline"
+          }
+        >
+          Most Liked
+        </div>
+      </div>
+      <div className="flex flex-col md:items-center md:gap-9 gap-2 relative w-full">
+        {currentTab == 0 &&
+          videoList
+            .sort(compareByRecency)
+            .map((vid, index) => 
+            <div  key={index} className="flex-col flex items-center xl:gap-9 gap-1 xl:relative max-w-[862px] md:w-full">
+
+              <StatVideoCard video={vid} />
+            </div>
+            )}
+        {currentTab == 1 &&
+          videoList
+            .sort(compareByPlays)
+            .reverse()
+            .map((vid, index) => (
+              <div
+                key={index}
+                className="flex-col flex items-center xl:gap-9 gap-1 xl:relative max-w-[862px] md:w-full"
+              >
+                <StatVideoCard video={vid} />
+                <div className="xl:absolute xl:text-2xl xl:-left-[26%] xl:top-9 text-xl font-extrabold">{`Plays: ${vid.plays}`}</div>
+              </div>
+            ))}
+        {currentTab == 2 &&
+          videoList
+            .sort(compareByLikes)
+            .reverse()
+            .map((vid, index) => (
+              <div
+                key={index}
+                className="flex-col flex items-center xl:gap-9 gap-1 xl:relative max-w-[862px] md:w-full"
+              >
+                <StatVideoCard video={vid} />
+                <div className="xl:absolute xl:text-2xl xl:-left-[26%] xl:top-9 text-xl font-extrabold">{`Likes: ${vid.likes}`}</div>
+              </div>
+            ))}
+      </div>
     </div>
-  )*/
+  );
 }
