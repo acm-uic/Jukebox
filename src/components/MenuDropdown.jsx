ad1f33e3aed5cee4e063cbceb80dc54d851e03bf
revenge789 <128012388+revenge789@users.noreply.github.com>
2024-04-14 23:27:17 -0500

    Added a smoother animation for mobile menu and removed the need to click the menu button in desktop devices

--- src/components/MenuDropdown.jsx
+++ src/components/MenuDropdown.jsx
@@ -1,22 +1,34 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
-export default function MenuDropdown() {
+export default function MenuDropdown({ showMenu, setShowMenu }) {
   return (
-    <ul className="lg:gap-4 lg:min-w-[240px] lg:static lg:flex-row lg:bg-transparent lg:text-white lg:py-0 lg:text-base lg:w-auto absolute left-0 top-[96px] bg-gray-400 w-full flex flex-col items-center gap-1 py-1 text-xl text-gray-300">
-      <li className="hover:text-white hover:underline w-full py-2">
+    <ul
+      className={`lg:hidden z-[5] fixed left-0 duration-100 - ${
+        showMenu ? "top-[96px]" : "top-[-10%]"
+      } bg-gray-400 w-full flex flex-col items-center gap-1 py-1 text-xl text-gray-300`}
+    >
+      <li
+        className="hover:text-white hover:underline w-full py-2"
+        onClick={() => setShowMenu(false)}
+      >
         <Link className="w-full flex justify-center" to="/">
           Home
         </Link>
       </li>
-      <div className="border w-full lg:hidden" />
-      <li className="hover:text-white hover:underline w-full py-2">
+      <div className="border w-full" />
+      <li
+        className="hover:text-white hover:underline w-full py-2"
+        onClick={() => setShowMenu(false)}
+      >
         <Link className="w-full flex justify-center" to="/stats">
           Stats
         </Link>
       </li>
-      <div className="border w-full lg:hidden" />
-      <li className="hover:text-white hover:underline w-full py-2">
+      <div className="border w-full" />
+      <li
+        className="hover:text-white hover:underline w-full py-2"
+        onClick={() => setShowMenu(false)}
+      >
         <Link className="w-full flex justify-center" to="/contributors">
           Contributors
         </Link>
